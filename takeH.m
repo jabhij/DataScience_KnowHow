2f63d737ee7ff0926eb54df9d9cd392d56bf090d
Abhishek <jaisabhii@gmail.com>
2023-11-27 16:27:09 -0600

    Create takeH.m

---
+++ takeH.m
@@ -0,0 +1,11 @@
+% Initialize option pricing parameters
+ScrambleDigitalNet = optPrice(ScrambleIID);
+ScrambleDigitalNet.priceParam.cubMethod = 'scrambledigitalnet'; % Use scrambled digital net
+[ScrambleDigitalNetPrice, outScrambleDigitalNet] = genOptPrice(ScrambleDigitalNet);
+
+% Display results
+disp(['Price of down and in put option using Scramble Digital Net Sampling : $' num2str(ScrambleDigitalNetPrice) ' +/- $' num2str(max(ScrambleDigitalNet.priceParam.absTol, ...
+ScrambleDigitalNet.priceParam.relTol*ScrambleDigitalNetPrice)) ])
+disp(['Time taken (in seconds): ' num2str(outScrambleDigitalNet.time)])
+disp(['Time saved from IID Sampling (in seconds): ' num2str(out.time - outScrambleDigitalNet.time)])
+disp(['Number of Paths saved from IID Sampling : ' num2str(out.nPaths - outScrambleDigitalNet.nPaths)])
